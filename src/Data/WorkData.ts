69b09e941d0d3a9baec05811e1f7d1d1081fef27
Pintu <pintu@Mac.lan>
2025-04-16 22:55:54 +0530

    update work details

--- src/Data/WorkData.ts
+++ src/Data/WorkData.ts
@@ -1,14 +1,24 @@
 const work=[
     // sample data format
     {
+      "image": "https://i.postimg.cc/K8QFJGFw/1705389198122.jpg",
+      "type":"WFH",
+      "title": "Full Stack Developer",
+      "company":"FreeLancer",
+      "duration":"Mar. 2025 - Present",
+      "location":"Full Time",
+      "body":"I specialize in designing and developing web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js) along with Next.js, creating responsive, user-friendly, and high-performance solutions. I collaborate closely with clients to understand their requirements and deliver tailored web applications that align with their goals. Additionally, I handle deployment and maintenance on cloud platforms, ensuring scalability, reliability, and adherence to best development practices.",
+      "user_id":1
+    },
+    {
     "image": "https://i.postimg.cc/K8QFJGFw/1705389198122.jpg",
     "type":"WFH",
-    "title": "Software Developer",
+    "title": "Full Stack Developer",
     "company":"Soluperts.ca",
-    "duration":"Aug. 2024 - Present",
+    "duration":"Aug. 2024 - Feb. 2025",
     "location":"Full Time",
     "body":"Implementing the solution using MongoDB, Express.js, React.js,JavaScript and Node.js to create a scalable web system. This includes designing role-based access control, data consolidation, reporting, and user interfaces.Implementing the solution using MongoDB, Express.js, React.js,JavaScript and Node.js to create a scalable web system. This includes designing role-based access control, data consolidation, reporting, and user interfaces.",
-    "user_id":1
+    "user_id":2
   },
   {
     "image": "https://i.postimg.cc/K8QFJGFw/1705389198122.jpg",
@@ -18,7 +28,7 @@ const work=[
     "duration":"Jan. 2024 - July 2024",
     "location":"Internship",
     "body":"Implementing the solution using MongoDB, Express.js, React.js,JavaScript and Node.js to create a scalable web system. This includes designing role-based access control, data consolidation, reporting, and user interfaces.Implementing the solution using MongoDB, Express.js, React.js,JavaScript and Node.js to create a scalable web system. This includes designing role-based access control, data consolidation, reporting, and user interfaces.",
-    "user_id":2
+    "user_id":3
   },
 
     {
@@ -29,7 +39,7 @@ const work=[
         "duration":"Aug. 2023 - Oct. 2023",
         "location":"Internship",
         "body":"I developed the front-end of an ICU digitization website using React and Bootstrap, connected it to the back-end with JavaScript, and resolved Cors errors in the ISAPI Protocol, improving website functionality",
-        "user_id":3
+        "user_id":4
       },
       {
         "image": "/Images/mantra.jpeg",
@@ -39,7 +49,7 @@ const work=[
         "duration":"Jan. 2023 - Jun. 2023",
         "location":"Internship",
         "body":"As a Frontend Developer Intern at Novus Aurelius, I designed responsive UIs with ReactJS, integrated real-time stock data, implemented interactive graphs, and optimized key sections like Blog and Pricing to enhance user experience and performance across devices.",
-        "user_id":4
+        "user_id":5
       },
 
       {
@@ -51,7 +61,7 @@ const work=[
         "location":"Internship",
         "body":"E-commerce, powered by mobile technology, reshapes global shopping habits. 'thecart,' an Android app, simplifies online shopping with product sections, banners, tracking, and Firebase data management, fueling the rapid growth of the E-commerce market.",
         // "projectlink":"https://github.com/rajandu/thecart2",
-        "user_id":5
+        "user_id":6
 
     }
 
